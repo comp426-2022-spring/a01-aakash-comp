d1cb3e464184506f4543a31430e1b79f61c8b729
Aakash Subramaniam <aakash99@email.unc.edu>
2022-04-20 23:28:29 -0400

    full

--- server.js
+++ server.js
@@ -43,12 +43,18 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./www/index.html.
 
-
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+})
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
+server.listen(port, () =>{
+    console.log(`Server running at port ${port}`)
+})
 
 // That's it! You're all done!
